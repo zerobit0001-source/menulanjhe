bf2f3b6abbb0cf7f26c28c32b94766878490b196
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-07 23:14:39 +0330

    Create restaurants admin

--- backend/apps/restaurants/admin.py
+++ backend/apps/restaurants/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import RestaurantProfile
 
-# Register your models here.
+
+@admin.register(RestaurantProfile)
+class RestaurantProfileAdmin(admin.ModelAdmin):
+    list_display = ["name", "tenant", "phone"]
