c3debddee4b1521e20a11a747b38b8cfbf485403
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-07 23:07:48 +0330

    Create branches view

--- backend/apps/branches/views.py
+++ backend/apps/branches/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from apps.common.mixins import TenantScopedQuerySetMixin
+from apps.tenants.permissions import HasTenantPermission
+from .models import Branch
+from .serializers import BranchSerializer
 
-# Create your views here.
+
+class BranchViewSet(TenantScopedQuerySetMixin, viewsets.ModelViewSet):
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+    tenant_lookup = "tenant"
+    permission_classes = [HasTenantPermission]
+    permission_codenames = {
+        "create": "settings.manage",
+        "update": "settings.manage",
+        "partial_update": "settings.manage",
+        "destroy": "settings.manage",
+    }
+    required_permission = "settings.view"
+
+    def perform_create(self, serializer):
+        serializer.save(tenant=self.request.tenant)
