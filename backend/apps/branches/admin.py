18274f6fe696efede6e994c0dc960689256512b3
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-07 23:09:52 +0330

    Branches migrations

--- backend/apps/branches/admin.py
+++ backend/apps/branches/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Branch
 
-# Register your models here.
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ["name", "tenant", "is_active", "is_default"]
+    list_filter = ["is_active", "is_default"]
+    search_fields = ["name"]
