1c44e19d0e85de356b45bc16ae331b1d2f828a38
amirrezaabdolahi <amirenzevadev@gmail.com>
2026-09-08 17:29:46 +0330

    feat(dashboard - dashboard page categories) : created the categories ui for dasboard page

--- frontend/src/features/dashboard/components/SectionTitle.tsx
+++ frontend/src/features/dashboard/components/SectionTitle.tsx
@@ -1,22 +1,40 @@
 import { Typography } from "@mui/material";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export const SectionTitle = ({
   title,
   icon,
   count,
+  link,
+  linkText,
 }: {
   title: string;
   icon: React.ReactNode;
   count?: number;
+  link?: string;
+  linkText?: string;
 }) => {
   return (
-    <div className="w-full flex items-center gap-2">
-      {icon}
-      <Typography variant="h6" className="font-light!">{title}</Typography>
-      {count !== undefined && (
-        <span className="bg-gray-200/50 size-8 flex items-center justify-center text-sm rounded-full">
-          {count}
-        </span>
+    <div className="w-full flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        {icon}
+        <Typography variant="h6" className="font-light!">
+          {title}
+        </Typography>
+        {count !== undefined && (
+          <span className="bg-gray-200/50 size-8 flex items-center justify-center text-sm rounded-full">
+            {count}
+          </span>
+        )}
+      </div>
+      {link && (
+        <Link
+          href={link}
+          className="flex items-center gap-1 text-sm text-blue-500 hover:underline hover:text-blue-600 transition-all"
+        >
+          {linkText} <ChevronLeft size={20} />{" "}
+        </Link>
       )}
     </div>
   );
--- frontend/src/features/dashboard/sections/DashboardPageCategories.tsx
+++ frontend/src/features/dashboard/sections/DashboardPageCategories.tsx
@@ -1,45 +1,69 @@
 import { Layers, Plus } from "lucide-react";
+import { Button, Card, Typography } from "@mui/material";
+
 import { SectionTitle } from "../components/SectionTitle";
 import { dasboardCtegories } from "../data/demoDashboard";
-import { Button, Card, Typography } from "@mui/material";
 
 export const DashboardPageCategories = () => {
   return (
-    <div>
+    <section>
       <SectionTitle
         title="دسته‌بندی‌ها"
         icon={<Layers size={20} className="text-gray-500" />}
-        count={5}
+        count={dasboardCtegories.menu_categories.length}
+        link="/dashboard/categories"
+        linkText="مشاهده همه"
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
-        <Button className="flex items-center justify-center h-30 bg-gray-200/50!">
-          <Typography
-            variant="h6"
-            className="text-lg! flex items-center"
-            color="textDisabled"
-          >
-            {" "}
-            <Plus size={20} /> دسته‌بندی‌
+
+      <div className="grid grid-cols-1 gap-4 py-4 md:grid-cols-2 lg:grid-cols-3">
+        {/* Add Category */}
+        <Button
+          variant="contained"
+          className="flex h-30! items-center justify-center"
+          sx={{
+            backgroundColor: "#F3F4F6",
+            color: "#9CA3AF",
+            boxShadow: "none",
+
+            "&:hover": {
+              backgroundColor: "#E5E7EB",
+              boxShadow: "none",
+            },
+          }}
+        >
+          <Typography variant="h6" className="flex items-center gap-2 text-lg!">
+            <Plus size={20} />
+            دسته‌بندی جدید
           </Typography>
         </Button>
+
+        {/* Categories */}
         {dasboardCtegories.menu_categories.map((category) => (
           <Card
             key={category.id}
-            className={`flex items-center gap-2 p-4 h-30 bg-[${category.style.background_color}]!`}
             elevation={3}
+            className="flex h-30! items-center justify-between rounded-2xl! p-4"
+            sx={{
+              backgroundColor: category.style.background_color,
+              color: category.style.text_color,
+            }}
           >
             <Typography
               variant="h6"
-              className="text-lg! flex items-center text-white!"
+              className="text-lg!"
+              sx={{
+                color: category.style.text_color,
+              }}
             >
               {category.title}
             </Typography>
-            <span className=" py-1 px-2 flex items-center justify-center rounded-full bg-black/50 text-white text-sm">
-              <p>{category.count} عدد</p>
+
+            <span className="rounded-full bg-black/20 px-3 py-1 text-sm text-white">
+              {category.count} عدد
             </span>
           </Card>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
