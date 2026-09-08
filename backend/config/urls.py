722844b85881c97bd2e4c5b31542081be2dd34e6
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-08 17:19:19 +0330

    fix root url config bug

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path("api/v1/admin/", include("apps.branches.urls")),
     path("api/v1/admin/", include("apps.menus.urls")),
     path("api/v1/admin/", include("apps.products.urls")),
-    path("api/v1/admin/", include("apps.menus.urls_public")),
+    path("api/v1/public/", include("apps.menus.urls_public")),
 ]
