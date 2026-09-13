5d6c80a4f2f22f55977852c90612339664e7eebd
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-13 17:02:24 +0330

    Add pagination config

---
+++ backend/apps/common/pagination.py
@@ -0,0 +1,16 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class DefaultPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,
+            "total_pages": self.page.paginator.num_pages,
+            "current_page": self.page.number,
+            "results": data,
+        })
