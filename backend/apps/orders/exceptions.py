6993f0aebcd3dd706f795595c4c8d35d6b29cfa6
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-09-19 13:51:33 +0330

    Update order exp

--- backend/apps/orders/exceptions.py
+++ backend/apps/orders/exceptions.py
@@ -1,3 +1,19 @@
+# from apps.common.exceptions import ApplicationError
+# 
+# 
+# class InvalidStateTransition(ApplicationError):
+#     default_code = "INVALID_STATE_TRANSITION"
+# 
+# 
+# class InvalidTableSession(ApplicationError):
+#     default_code = "INVALID_TABLE_SESSION"
+#     status_code = 400
+# 
+# 
+# class ProductNotAvailable(ApplicationError):
+#     default_code = "PRODUCT_UNAVAILABLE"
+#     status_code = 400
+
 from apps.common.exceptions import ApplicationError
 
 
@@ -5,8 +21,8 @@ class InvalidStateTransition(ApplicationError):
     default_code = "INVALID_STATE_TRANSITION"
 
 
-class InvalidTableSession(ApplicationError):
-    default_code = "INVALID_TABLE_SESSION"
+class InvalidTable(ApplicationError):
+    default_code = "INVALID_TABLE"
     status_code = 400
 
 
